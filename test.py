40265de2ef8a5f201a34fe8614e384fda5d57102
al14twi <n1t_@outlook.com>
2024-10-15 14:45:26 +0100

    update ig

---
+++ test.py
@@ -0,0 +1,75 @@
+print("i hate niggers!")
+print("theyre really black!")
+# test
+
+name1 = "Flare"
+food = "dick"
+email = "cockmuncher123@py.co"
+age = 69
+quantity = 3
+pupils = 69
+price = 69.99
+gpa = 0.3
+distance = 7472.8
+is_student = True 
+for_sale = True
+
+print(f"wsg {name1}")
+print(f"you like {food} right?, choke on this then")
+print(f"idk why but im obligated to tell you your own fake email which is:{email}.")
+print(f"you;re {age} :drool:")
+print(f"you are buying {quantity} naughty toys")
+print(f"your class has {pupils} students")
+print(f"the price is !{price} each")
+print(f"your gpa is {gpa}")
+print(f"you stayed still {distance}km")
+print(f"are you wet?: {is_student}")
+
+is_online = True
+
+if is_student:
+    print("you hate niggers!")
+else:
+    print("you are a nigger.. grr")
+
+if for_sale:
+    print("this black slave is for sale!")
+else: 
+    print("Sorry! this slave is already reserved.")
+
+if is_online:
+    print(f"{name1} is online!")
+else:
+    print(f"{name1} is not online.")
+
+
+print(type(email))
+print(type(age))
+print(type(gpa))
+print(type(is_online))
+
+gpa = int(gpa)
+age = float(age)
+age = str(age)
+name = bool(name1)
+
+print(gpa)
+print(age)
+print(name)
+
+uid = input("What is your username?:  ")
+print(f"Welcome {uid}!")
+
+bc = input("What is your bodycount?")
+bc = float(bc)
+if bc:
+    print("Wow im actually surprised you managed to bag someone xD i didnt have very high hopes for you yk")
+else:
+    print("VIRGINNN BOYYYY!!! AHAHAHAHAHAHAHAHAHAHAHAH LOWER YOUR TONE IN MY PRESENCE")
+
+
+
+
+
+length = input("Length: ")
+width = input("Width:  ")
