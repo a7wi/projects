ce8ad5c2a6f947e088e50bdf8bdd89cfe6cddaf0
a7wi <107149328+a7wi@users.noreply.github.com>
2025-04-02 04:00:02 +0100

    e

---
+++ python/newtest.py
@@ -0,0 +1,19 @@
+4386 * 4,
+print(''' omg
+ yall are acutal fucking retards
+      ''')
+
+
+
+tax = 12.5 / 100
+
+price = 100.50
+
+tax_amount = price * tax
+
+print(price + tax_amount)
+
+print(0b101010)
+
+
+print(len("And now for something completely different"))
